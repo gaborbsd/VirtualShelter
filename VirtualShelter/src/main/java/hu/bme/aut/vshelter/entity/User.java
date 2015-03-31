351062cd664df26fa6d03ecc6d13f1065ded1149
Gábor Kövesdán <gabpr@kovesdan.org>
2015-03-31 18:26:02 +0200

    - Use String set for roles

--- VirtualShelter/src/main/java/hu/bme/aut/vshelter/entity/User.java
+++ VirtualShelter/src/main/java/hu/bme/aut/vshelter/entity/User.java
@@ -1,49 +1,50 @@
 package hu.bme.aut.vshelter.entity;
 
+import java.util.Collections;
 import java.util.Set;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user", catalog = "vshelter")
 public class User extends Advertiser {
-	
+
 	@Column(name = "password", unique = false, nullable = false)
 	private String password;
-	
+
 	@Column(name = "introductionText", unique = false, nullable = true)
 	private String introductionText;
-	
-	@ManyToMany
-	private Set<Role> roles;
-	
+
+	@ElementCollection
+	private Set<String> roles;
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getIntroductionText() {
 		return introductionText;
 	}
-	
+
 	public void setIntroductionText(String introductionText) {
 		this.introductionText = introductionText;
 	}
 	
-	public Set<Role> getRoles() {
-		return roles;
+	public Set<String> getRoles() {
+		return Collections.unmodifiableSet(roles);
 	}
-	
-	public void setRoles(Set<Role> roles) {
+
+	public void setRoles(Set<String> roles) {
 		this.roles = roles;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
