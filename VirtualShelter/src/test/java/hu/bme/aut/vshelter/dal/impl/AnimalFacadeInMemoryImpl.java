78d9006aa87516f84d9dd2dff821f770fa9721ba
Kruták László <lazloz92@sch.bme.hu>
2015-04-03 19:53:15 +0200

    - AnimalFacadeInMemoryImpl prasing

--- VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/impl/AnimalFacadeInMemoryImpl.java
+++ VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/impl/AnimalFacadeInMemoryImpl.java
@@ -1,11 +1,13 @@
 package hu.bme.aut.vshelter.dal.impl;
 
 import hu.bme.aut.vshelter.dal.AnimalFacade;
+import hu.bme.aut.vshelter.entity.Advertisement;
 import hu.bme.aut.vshelter.entity.Animal;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class AnimalFacadeInMemoryImpl implements AnimalFacade {
 
@@ -23,19 +25,28 @@ public class AnimalFacadeInMemoryImpl implements AnimalFacade {
 
 	@Override
 	public Animal findAnimalById(long animalId) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return animals.stream()
+					.filter(a -> a.getId() == animalId).findFirst()
+					.get();
+		} catch (NoSuchElementException e) {
+			return null;
+		}
 	}
 
 	@Override
 	public void edit(Animal animal) {
-		// TODO Auto-generated method stub
+		animals
+			.set(animals.indexOf(animal), animal);
 		
 	}
 
 	@Override
 	public void deleteAnimalById(long animalId) {
-		// TODO Auto-generated method stub
-		
+		Animal deleteAnimal = findAnimalById(animalId);
+
+		if (deleteAnimal != null)
+			animals.remove(deleteAnimal);
+	
 	}
 }
