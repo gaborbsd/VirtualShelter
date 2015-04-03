de0c99b33506f4f9b5c05fc9599e301a2d83556f
Kruták László <lazloz92@sch.bme.hu>
2015-04-04 01:24:03 +0200

    -create institution test and memory implementation

--- VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/impl/InstitutionFacadeInMemoryImpl.java
+++ VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/impl/InstitutionFacadeInMemoryImpl.java
@@ -1,8 +1,51 @@
 package hu.bme.aut.vshelter.dal.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import hu.bme.aut.vshelter.dal.InstitutionFacade;
+import hu.bme.aut.vshelter.entity.Breed;
 import hu.bme.aut.vshelter.entity.Institution;
 
-public class InstitutionFacadeInMemoryImpl {
+public class InstitutionFacadeInMemoryImpl implements InstitutionFacade {
 
-	private Institution institution = new Institution();
+	private List<Institution> institutions = new ArrayList<Institution>();
+
+	@Override
+	public Institution findInstitutionById(long institutionId) {
+		try {
+			return institutions.stream()
+					.filter(a -> a.getId() == institutionId).findFirst()
+					.get();
+		} catch (NoSuchElementException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public List<Institution> findAll() {
+		return Collections.unmodifiableList(institutions);
+	}
+
+	@Override
+	public void create(Institution institution) {
+		institutions.add(institution);
+		
+	}
+
+	@Override
+	public void edit(Institution institution) {
+		institutions
+		.set(institutions.indexOf(institution), institution);
+	}
+
+	@Override
+	public void deleteInstitutionById(long institutionId) {
+		Institution deleteInstitution = findInstitutionById(institutionId);
+
+		if (deleteInstitution != null)
+			institutions.remove(deleteInstitution);
+	}
 }
