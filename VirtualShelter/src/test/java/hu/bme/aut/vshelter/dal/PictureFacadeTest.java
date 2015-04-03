3afe827a2aeefbd3ab0cb997f96f7fb3a4d47493
Kruták László <lazloz92@sch.bme.hu>
2015-04-04 01:51:43 +0200

    -create PictureFacade test and in memory implementation

---
+++ VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/PictureFacadeTest.java
@@ -0,0 +1,95 @@
+package hu.bme.aut.vshelter.dal;
+
+import static org.junit.Assert.*;
+import hu.bme.aut.vshelter.entity.Picture;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/test-config.xml" })
+public class PictureFacadeTest {
+	@Autowired
+	private PictureFacade pictureFacade;
+	
+	@Test
+	public void testPersist(){
+		Picture picture = new Picture();
+		picture.setId(1);
+
+		pictureFacade.create(picture);
+		List<Picture> pictures = pictureFacade.findAll();
+		assertTrue(pictures.contains(picture));
+	}
+	
+	@Test
+	public void testPersistNotAddedIsNotContains() {
+		Picture picture = new Picture();
+		picture.setId(2);
+		
+		List<Picture> pictures = pictureFacade.findAll();
+		assertFalse(pictures.contains(picture));
+	}
+	
+	@Test
+	public void testFindAdvertisementById(){
+		Picture picture = new Picture();
+		Picture actual;
+		
+		picture.setId(3);
+		
+		pictureFacade.create(picture);
+		
+		actual = pictureFacade.findPictureById(3);
+		assertEquals(picture, actual);
+		assertEquals(picture.getId(), actual.getId());
+	}
+	
+	@Test
+	public void testNoFindPictureByNotExistId(){
+		Picture actual;
+		actual = pictureFacade.findPictureById(4012);
+		assertNull(actual);
+	}
+	
+	@Test
+	public void testEditPicturet(){
+		Picture picture = new Picture();
+
+		picture.setId(4);
+		
+		pictureFacade.create(picture);
+		
+		Picture modified = new Picture();
+
+		modified.setId(4);
+		
+		pictureFacade.edit(modified);
+		
+		Picture actual;
+		actual = pictureFacade.findPictureById(4);
+		assertEquals(modified, actual);
+
+		assertEquals(picture.getId(), actual.getId());
+	}
+	
+	@Test
+	public void testDeletePictureById(){
+		Picture picture = new Picture();
+
+		picture.setId(5);
+		
+		pictureFacade.create(picture);
+		
+		pictureFacade.deletePictureById(5);
+		
+		Picture actual;
+		actual = pictureFacade.findPictureById(5);
+		assertNull(actual);
+	}
+}
--- VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/impl/PictureFacadeInMemoryImpl.java
+++ VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/impl/PictureFacadeInMemoryImpl.java
@@ -1,8 +1,49 @@
 package hu.bme.aut.vshelter.dal.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import hu.bme.aut.vshelter.dal.PictureFacade;
 import hu.bme.aut.vshelter.entity.Picture;
 
-public class PictureFacadeInMemoryImpl {
+public class PictureFacadeInMemoryImpl implements PictureFacade {
 
-	private Picture picture = new Picture();
+	private List<Picture> pictures = new ArrayList<Picture>();
+
+	@Override
+	public Picture findPictureById(long pictureId) {
+		try {
+			return pictures.stream()
+					.filter(a -> a.getId() == pictureId).findFirst()
+					.get();
+		} catch (NoSuchElementException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public List<Picture> findAll() {
+		return Collections.unmodifiableList(pictures);
+	}
+
+	@Override
+	public void create(Picture picture) {
+		pictures.add(picture);
+	}
+
+	@Override
+	public void edit(Picture picture) {
+		pictures
+			.set(pictures.indexOf(picture), picture);
+	}
+
+	@Override
+	public void deletePictureById(long pictureId) {
+		Picture deletePicture = findPictureById(pictureId);
+
+		if (deletePicture != null)
+			pictures.remove(deletePicture);
+	}
 }
