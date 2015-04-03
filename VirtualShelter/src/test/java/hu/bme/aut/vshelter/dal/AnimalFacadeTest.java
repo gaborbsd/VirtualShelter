85438427c2ab2a75bd1d6d6c8b666b13baa9f761
Kruták László <lazloz92@sch.bme.hu>
2015-04-03 20:23:31 +0200

    - AnimalFacadeTest phrasing

--- VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/AnimalFacadeTest.java
+++ VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/AnimalFacadeTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import hu.bme.aut.vshelter.entity.Animal;
-import hu.bme.aut.vshelter.entity.User;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,13 +19,6 @@ public class AnimalFacadeTest {
 	private AnimalFacade animalFacade;
 	
 	@Test
-	public void testDI() {
-		assertNotNull(animalFacade);
-		assertEquals(animalFacade.getClass().getSimpleName(),
-				"AnimalFacadeInMemoryImpl");
-	}
-	
-	@Test
 	public void testPersist() {
 		Animal animal = new Animal();
 		animal.setName("Cirmi");
@@ -34,5 +26,77 @@ public class AnimalFacadeTest {
 		animalFacade.create(animal);
 		List<Animal> animals = animalFacade.findAll();
 		assertTrue(animals.contains(animal));
+	}
+	
+	@Test
+	public void testPersistNotAddedIsNotContains() {
+		Animal animal = new Animal();
+		animal.setName("Jankó");
+		animal.setId(2);
+		
+		List<Animal> animals = animalFacade.findAll();
+		assertFalse(animals.contains(animal));
+	}
+	
+	@Test
+	public void testFindAdvertisementById(){
+		Animal animal = new Animal();
+		Animal actual;
+		animal.setName("Kormos");
+		animal.setDescription("morcos fekete cica");
+		animal.setId(3);
+		
+		
+		animalFacade.create(animal);
+		
+		actual = animalFacade.findAnimalById(3);
+		assertEquals(animal, actual);
+		assertEquals(animal.getName(), actual.getName());
+		assertEquals(animal.getDescription(), actual.getDescription());
+		assertEquals(animal.getId(), actual.getId());
+
+	}
+	
+	@Test
+	public void testNoFindAnimalByNotExistId(){
+		Animal actual;
+		actual = animalFacade.findAnimalById(4012);
+		assertNull(actual);
+	}
+	
+	@Test
+	public void testEditAnimalt(){
+		Animal animal = new Animal();
+		animal.setName("Vörös");
+		animal.setId(4);
+		
+		animalFacade.create(animal);
+		
+		Animal modified = new Animal();
+		modified.setName("Vörös");
+		animal.setDescription("aranyos vörös cica");
+		modified.setId(4);
+		
+		Animal actual;
+		actual = animalFacade.findAnimalById(4);
+		assertEquals( modified, actual);
+		assertEquals(modified.getName(), actual.getName());
+		assertEquals(modified.getDescription(), actual.getDescription());
+		assertEquals(animal.getName(), actual.getName());
+	}
+	
+	@Test
+	public void testDeleteAnimalById(){
+		Animal animal = new Animal();
+		animal.setName("Fehérke");
+		animal.setId(5);
+		
+		animalFacade.create(animal);
+		
+		animalFacade.deleteAnimalById(5);
+		
+		Animal actual;
+		actual = animalFacade.findAnimalById(5);
+		assertNull(actual);
 	}
 }
