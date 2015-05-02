beb3d230edc35c2192a5df23f0da49563e775093
lazloz92 <lazloz92@gmail.com>
2015-05-03 00:42:04 +0200

    done - AddressFacadeMockTest

--- VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/AddressFacadeMockTest.java
+++ VirtualShelter/src/test/java/hu/bme/aut/vshelter/dal/AddressFacadeMockTest.java
@@ -1,8 +1,8 @@
 package hu.bme.aut.vshelter.dal;
 
-import static org.easymock.EasyMock.createNiceMock;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
@@ -14,74 +14,167 @@ import hu.bme.aut.vshelter.dal.impl.AddressFacadeJPAImpl;
 import hu.bme.aut.vshelter.entity.Address;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class AddressFacadeMockTest {
 
 	private AddressFacadeJPAImpl addressFacade;
 	private EntityManager mockEm;
-	private TypedQuery<Address> mockQuery;
-	
-	  @Before
-	  @SuppressWarnings("unchecked")
-	  public void setUp() throws Exception {
-	    // NiceMocks return default values for
-	    // unimplemented methods
-		mockEm = createNiceMock(EntityManager.class);
-		mockQuery = (TypedQuery<Address>)createMock(TypedQuery.class);
-	    addressFacade = new AddressFacadeJPAImpl(mockEm);
-	  }
+	private TypedQuery<Address> mockTypedQuery;
+	private Query mockQuery;
 
-	  @Test
-	  public void findAddressByIdTest() {
-	    
+	@Before
+	@SuppressWarnings("unchecked")
+	public void setUp() throws Exception {
+		mockEm = createMock(EntityManager.class);
+		mockTypedQuery = (TypedQuery<Address>) createMock(TypedQuery.class);
+		mockQuery = createMock(Query.class);
+		addressFacade = new AddressFacadeJPAImpl(mockEm);
+	}
+
+	@Test
+	public void findAddressByIdShouldReturnAddressTest() {
 		Address address = new Address();
 		address.setId(1);
 		address.setAddress("alma street");
 		address.setCity("Nagyvárad");
 		address.setCountry("Hungary");
-		
-		// Setting up the expected value of the method call find
-	    expect(mockEm.find(Address.class, address.getId())).andReturn(address).times(1);
-	    
-	    // Setup is finished need to activate the mock
-	    replay(mockEm);
-	    
-	    addressFacade.create(address); // add an address 
-	    assertEquals(address, addressFacade.findAddressById(address.getId()));
 
-	    verify(mockEm);
-	  }
-	  
-	 
-	  @Test
-	  public void findAllAnimalTest() {
+		mockEm.persist(address);
+		expectLastCall();
+		expect(mockEm.find(Address.class, address.getId())).andReturn(address);
+
+		replay(mockEm);
+
+		addressFacade.create(address);
+		assertEquals(address, addressFacade.findAddressById(address.getId()));
+
+		verify(mockEm);
+	}
+
+	@Test
+	public void findAddressByIdShouldReturnNullTest() {
+		expect(mockEm.find(Address.class, (long) 1)).andReturn(null);
+
+		replay(mockEm);
+
+		assertEquals(null, addressFacade.findAddressById((long) 1));
+
+		verify(mockEm);
+	}
+
+	@Test
+	public void findAllAddressShouldReturnAllAddressTest() {
 		Address address = new Address();
-		address.setId(1);
+		address.setId(2);
 		address.setAddress("alma street");
 		address.setCity("Nagyvárad");
 		address.setCountry("Hungary");
 		List<Address> addresses = new ArrayList<Address>();
 		addresses.add(address);
+
+		mockEm.persist(address);
+		expectLastCall();
+		expect(mockEm.createQuery("SELECT a FROM Address a", Address.class))
+				.andReturn(mockTypedQuery);
+		expect(mockTypedQuery.getResultList()).andReturn(addresses);
+
+		replay(mockEm);
+		replay(mockTypedQuery);
+
+		addressFacade.create(address);
+		assertEquals(addresses, addressFacade.findAll());
+
+		verify(mockEm);
+		verify(mockTypedQuery);
+	}
+
+	@Test
+	public void findAllAddressShouldReturnNullTest() {
+		expect(mockEm.createQuery("SELECT a FROM Address a", Address.class))
+				.andReturn(mockTypedQuery);
+		expect(mockTypedQuery.getResultList()).andReturn(null);
+
+		replay(mockEm);
+		replay(mockTypedQuery);
+
+		assertEquals(null, addressFacade.findAll());
+
+		verify(mockEm);
+		verify(mockTypedQuery);
+	}
+
+	@Test
+	public void createAddressTest() {
+		Address address = new Address();
+		address.setId(3);
+		address.setAddress("alma street");
+		address.setCity("Nagyvárad");
+		address.setCountry("Hungary");
+
+		mockEm.persist(address);
+		expectLastCall();
+		replay(mockEm);
+
+		addressFacade.create(address);
+		verify(mockEm);
+	}
+
+	@Test
+	public void editAddressShouldModifieorCreateAddressTest() {
+		Address address = new Address();
+		address.setId(3);
+		address.setAddress("alma street");
+		address.setCity("Nagyvárad");
+		address.setCountry("Hungary");
+
+		expect(mockEm.merge(address)).andReturn(address);
+		replay(mockEm);
+
+		addressFacade.edit(address);
+		verify(mockEm);
+	}
+
+	@Test
+	public void deletAddressByIdDeleteTheAddress() {
+		Address address = new Address();
+		address.setId(6);
+		address.setAddress("alma street");
+		address.setCity("Nagyvárad");
+		address.setCountry("Hungary");
+		List<Address> addresses = new ArrayList<Address>();
+		addresses.add(address);
+
+		mockEm.persist(address);
+		expectLastCall();
+
+		expect(mockEm.createQuery("DELETE FROM Address where id=:p"))
+				.andReturn(mockQuery);
+		expect(mockQuery.setParameter("p", address.getId()))
+				.andReturn(mockQuery);
+
+		expect(mockQuery.executeUpdate()).andReturn(1);
+
+		expect(mockEm.createQuery("SELECT a FROM Address a", Address.class))
+				.andReturn(mockTypedQuery);
+		expect(mockTypedQuery.getResultList()).andReturn(null);
+
+		replay(mockEm);
+		replay(mockQuery);
+		replay(mockTypedQuery);
+
+		addressFacade.create(address);
 		
-		// Setting up the expected value of the method createQuery and getResultList
-		expect(mockEm.createQuery("SELECT a FROM Address a",
-				Address.class)).andReturn(mockQuery);
-		expect(mockQuery.getResultList()).andReturn(addresses);
+		addressFacade.deleteAddressById(address.getId());
 		
-		// Setup is finished need to activate the mock
-	    replay(mockEm);
-	    replay(mockQuery);
-	    
-	    addressFacade.create(address); // add an address
-	    assertEquals(addresses, addressFacade.findAll());
-	    
-	    verify(mockEm);
-	    verify(mockQuery);
-	  }
-	
+		assertEquals(null, addressFacade.findAll());
+
+		verify(mockEm);
+		verify(mockQuery);
+		verify(mockTypedQuery);
+	}
 }
