31273b891ee7770fd1f4745c00b3ced101c7fc1e
barni <barni@wanari.com>
2015-05-07 23:09:10 +0200

    animal,shelter and user editor done

--- VirtualShelter/src/main/webapp/js/controllers.js
+++ VirtualShelter/src/main/webapp/js/controllers.js
@@ -1,15 +1,42 @@
-var app = angular.module('vshelterApp', []);
+var app = angular.module('vshelterApp', ['ngRoute','bgf.paginateAnything']);
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/editor/user',{
+		controller: 'UserEditorController',
+		templateUrl: '/html/userform.html'
+	}).when('/editor/user/:id',{
+		controller: 'UserEditorController',
+		templateUrl: '/html/userform.html'
+	}).when('/editor/animal/:id',{
+		controller: 'AnimalEditorController',
+		templateUrl: '/html/animalform.html'
+	}).when('/editor/animal',{
+		controller: 'AnimalEditorController',
+		templateUrl: '/html/animalform.html'
+	}).when('/editor/shelter',{
+		controller: 'ShelterEditorController',
+		templateUrl: '/html/shelterform.html'
+	}).when('/editor/shelter/:id',{
+		controller: 'ShelterEditorController',
+		templateUrl: '/html/shelterform.html'
+	}).when('/',{
+		
+	})
+})
 
 app.controller('AnimalController', function($scope, $http) {
-	$scope.getAnimals = function() {
+	$scope.getAnimals = getAnimals;
+	$scope.postAnimal = postAnimal;
+	
+	function getAnimals() {
 		$http.get('api/animal').success(function(data) {
 			$scope.animals = data;
 		});
 	};
 
-	$scope.postAnimal = function() {
+	function postAnimal() {
 		if ($scope.animal) {
-			$http.post('api/animal', $scope.animal)
+			$http.post('api/animal', vm.animal)
 					.success(function() {
 						$scope.getAnimals();
 						$scope.animal = null;
@@ -17,5 +44,36 @@ app.controller('AnimalController', function($scope, $http) {
 		}
 	};
 
-	$scope.getAnimals();
+	vm.getAnimals();
 });
+
+app.controller('HeaderController', function($scope, $http) {
+	$scope.loginForm = {}
+	$scope.login = login;
+	
+	function login(){}
+	
+	function register(){}
+	
+});
+
+app.controller('SearchController', function($scope, $http){
+	$scope.detailed = true;
+	
+});
+
+app.controller('UserEditorController', function($scope, $http){
+	
+})
+
+app.controller('AnimalEditorController', function($scope, $http){
+	$scope.animal = {};
+	$scope.animal.disabilities = false;
+	$scope.animal.diseases = false;
+	$scope.animal.otherCosts = false;
+})
+
+app.controller('ShelterEditorController', function($scope, $http){
+	
+})
+
