8eb9e6844a1600271ed8d070a4df68a20c680a38
badam17 <adambarsi17@gmail.com>
2016-05-16 15:41:16 +0200

    Add User and SportEvent entities

---
+++ VirtualShelter/src/main/java/hu/bme/aut/vshelter/entity/SportEvent.java
@@ -0,0 +1,82 @@
+package hu.bme.aut.vshelter.entity;
+
+import java.util.Calendar;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class SportEvent {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false)
+	private Calendar date;
+	
+	@ManyToOne
+	private User owner;
+	
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SportEvent other = (SportEvent) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
+
+}
---
+++ VirtualShelter/src/main/java/hu/bme/aut/vshelter/entity/User.java
@@ -0,0 +1,87 @@
+package hu.bme.aut.vshelter.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(unique=true)
+	private String name;
+	
+	@OneToMany(mappedBy="owner")
+	private List<SportEvent> events;
+	
+	private int age;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<SportEvent> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<SportEvent> events) {
+		this.events = events;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+}
