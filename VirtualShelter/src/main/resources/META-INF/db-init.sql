8b874e7a51cc6f5362d28c9e149d9ab8ae2efdd5
badam17 <adambarsi17@gmail.com>
2016-05-16 16:08:17 +0200

    init script modified

--- VirtualShelter/src/main/resources/META-INF/db-init.sql
+++ VirtualShelter/src/main/resources/META-INF/db-init.sql
@@ -1,3 +1,11 @@
 INSERT INTO animal VALUES (1, "Bundás");
 INSERT INTO animal VALUES (2, "Cirmi");
 INSERT INTO animal VALUES (3, "Artúr");
+
+INSERT INTO user VALUES (1, 34, "Andras");
+INSERT INTO user VALUES (2, 23, "Bela");
+INSERT INTO user VALUES (3, 27, "Cecil");
+
+INSERT INTO sportevent VALUES (1, '2016-08-18 10:00:00', "tenisz", 1);
+INSERT INTO sportevent VALUES (2, '2016-08-19 16:00:00', "tenisz", 1);
+INSERT INTO sportevent VALUES (3, '2016-07-13 16:00:00', "foci", 2);
