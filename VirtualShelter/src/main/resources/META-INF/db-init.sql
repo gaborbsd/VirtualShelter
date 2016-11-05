7008e5258d9a2a9a932a72a7c912e0373e681428
badam17 <adambarsi17@gmail.com>
2016-11-05 15:07:08 +0100

    Add extra users to init script

--- VirtualShelter/src/main/resources/META-INF/db-init.sql
+++ VirtualShelter/src/main/resources/META-INF/db-init.sql
@@ -1,12 +1,18 @@
 INSERT INTO address VALUES (1, "Petofi u. 16/A", "Szekszard", "Magyarorszag", "Tolna", 7100)
 INSERT INTO address VALUES (2, "Arany J. u. 44", "Kaposvar", "Magyarorszag", "Somogy", 7400)
 INSERT INTO address VALUES (3, "Honved u. 12/B", "Pecs", "Magyarorszag", "Baranya", 7630)
-INSERT INTO address VALUES (4, "Deak u. 16/B", "Kecskemet", "Magyarorszag", "Bacs-Kiskun", 4400)
+INSERT INTO address VALUES (4, "Deak u. 16/B", "Kecskemet", "Magyarorszag", "Bacs-Kiskun", 6000)
+INSERT INTO address VALUES (5, "Virag u. 23", "Kaposvar", "Magyarorszag", "Somogy", 7400)
+INSERT INTO address VALUES (6, "Dozsa Gyorgy u. 33", "Kecskemet", "Magyarorszag", "Bacs-Kiskun", 6000)
+INSERT INTO address VALUES (7, "Kolcsei Ferenc utca 4", "Miskolc", "Magyarorszag", "Borsod-Abauj-Zemplen", 3532)
 
 INSERT INTO user VALUES (1, 34, "andras1@gmail.com", "Andras", "a", "+36301234567",1);
 INSERT INTO user VALUES (2, 23, "bela2@gmail.com", "Bela", "b","+36201234567",2);
 INSERT INTO user VALUES (3, 27, "cecil3@gmail.com", "Cecil", "c", "+36701234567",3);
 INSERT INTO user VALUES (4, 27, "david4@gmail.com", "David", "d", "+36707654321",4);
+INSERT INTO user VALUES (5, 12, "elemer5@gmail.com", "Elemer", "e", "+36706254193",5);
+INSERT INTO user VALUES (6, 44, "ferenc6@gmail.com", "Ferenc", "f", "+36205291403",6);
+INSERT INTO user VALUES (7, 36, "gabor7@gmail.com", "Gabor", "g", "+36304954720",7);
 
 INSERT INTO sportevent VALUES (1, '2016-08-18 10:00:00', "tenisz",2, "TENNIS", 1);
 INSERT INTO sportevent VALUES (2, '2016-08-19 16:00:00', "tenisz",4, "TENNIS", 1);
